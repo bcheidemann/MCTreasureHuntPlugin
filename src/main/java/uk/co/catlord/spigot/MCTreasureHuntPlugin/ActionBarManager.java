9deb714c6ba6c40d52ccef59861b96e652b47031
Ben Heidemann <ben@heidemann.co.uk>
2023-03-05 20:40:39 +0000

    add ActionBarManager

---
+++ src/main/java/uk/co/catlord/spigot/MCTreasureHuntPlugin/ActionBarManager.java
@@ -0,0 +1,12 @@
+package uk.co.catlord.spigot.MCTreasureHuntPlugin;
+
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+
+public class ActionBarManager {
+    public static void sendActionBar(Player player, String message) {
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
+    }
+}
