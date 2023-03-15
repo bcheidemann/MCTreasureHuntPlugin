afefc33693f43da946415befa8a97c12803fb197
Ben Heidemann <ben@heidemann.co.uk>
2023-03-15 20:41:39 +0000

    format files

--- src/main/java/uk/co/catlord/spigot/MCTreasureHuntPlugin/ActionBarManager.java
+++ src/main/java/uk/co/catlord/spigot/MCTreasureHuntPlugin/ActionBarManager.java
@@ -1,12 +1,11 @@
 package uk.co.catlord.spigot.MCTreasureHuntPlugin;
 
-import org.bukkit.entity.Player;
-
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.entity.Player;
 
 public class ActionBarManager {
-    public static void sendActionBar(Player player, String message) {
-        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
-    }
+  public static void sendActionBar(Player player, String message) {
+    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
+  }
 }
--- src/main/java/uk/co/catlord/spigot/MCTreasureHuntPlugin/EventListener.java
+++ src/main/java/uk/co/catlord/spigot/MCTreasureHuntPlugin/EventListener.java
@@ -7,13 +7,16 @@ import org.bukkit.plugin.Plugin;
 
 // An example event listener to show how to use the ActionBarManager
 public class EventListener implements Listener {
-    public static void register(Plugin plugin) {
-        plugin.getServer().getPluginManager().registerEvents(new EventListener(), plugin);
-    }
+  public static void register(Plugin plugin) {
+    plugin.getServer().getPluginManager().registerEvents(new EventListener(), plugin);
+  }
 
-    @EventHandler
-    public void onPlayerMove(PlayerMoveEvent event) {
-        ActionBarManager.sendActionBar(event.getPlayer(), String.format("X: %d, Y: %d, Z: %d",
-                event.getTo().getBlockX(), event.getTo().getBlockY(), event.getTo().getBlockZ()));
-    }
+  @EventHandler
+  public void onPlayerMove(PlayerMoveEvent event) {
+    ActionBarManager.sendActionBar(
+        event.getPlayer(),
+        String.format(
+            "X: %d, Y: %d, Z: %d",
+            event.getTo().getBlockX(), event.getTo().getBlockY(), event.getTo().getBlockZ()));
+  }
 }
