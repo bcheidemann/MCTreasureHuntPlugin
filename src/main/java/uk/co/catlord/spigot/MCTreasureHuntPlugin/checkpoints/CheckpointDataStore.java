b11f4a84927eb86b6c45b32868f6b85ef5264e27
Ben Heidemann <56122437+bcheidemann@users.noreply.github.com>
2023-04-29 11:23:28 +0100

    feat: Compass target is next checkpoint (#30)

--- src/main/java/uk/co/catlord/spigot/MCTreasureHuntPlugin/checkpoints/CheckpointDataStore.java
+++ src/main/java/uk/co/catlord/spigot/MCTreasureHuntPlugin/checkpoints/CheckpointDataStore.java
@@ -3,6 +3,11 @@ package uk.co.catlord.spigot.MCTreasureHuntPlugin.checkpoints;
 import java.util.ArrayList;
 import java.util.List;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import uk.co.catlord.spigot.MCTreasureHuntPlugin.App;
@@ -11,9 +16,11 @@ import uk.co.catlord.spigot.MCTreasureHuntPlugin.errors.ErrorPathContext;
 import uk.co.catlord.spigot.MCTreasureHuntPlugin.errors.ErrorReport;
 import uk.co.catlord.spigot.MCTreasureHuntPlugin.errors.ErrorReportBuilder;
 import uk.co.catlord.spigot.MCTreasureHuntPlugin.errors.Result;
+import uk.co.catlord.spigot.MCTreasureHuntPlugin.player_tracker.PlayerData;
+import uk.co.catlord.spigot.MCTreasureHuntPlugin.player_tracker.PlayerTrackerDataStore;
 import uk.co.catlord.spigot.MCTreasureHuntPlugin.utils.JsonDataStore;
 
-public class CheckpointDataStore extends JsonDataStore {
+public class CheckpointDataStore extends JsonDataStore implements Listener {
   private static CheckpointDataStore instance;
   public ArrayList<Checkpoint> checkpoints = new ArrayList<>();
 
@@ -93,6 +100,8 @@ public class CheckpointDataStore extends JsonDataStore {
               "Failed to load checkpoints: " + e.getMessage()));
     }
 
+    register();
+
     return Result.ok(true);
   }
 
@@ -153,5 +162,60 @@ public class CheckpointDataStore extends JsonDataStore {
       names.add(checkpoint.name);
     }
     return names;
+  }
+
+  private void register() {
+    App.instance.getServer().getPluginManager().registerEvents(this, App.instance);
+  }
+
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent event) {
+    // The compass will not be updated until the player has joined. This even is fired before the
+    // player
+    // has properly joined the game so it seems to not update the compass target correctly.
+    Bukkit.getScheduler()
+        .runTaskLater(App.instance, () -> updateCompassForPlayer(event.getPlayer()), 1);
+  }
+
+  public void updateCompassForPlayer(Player player) {
+    Result<PlayerData, String> playerDataResult =
+        PlayerTrackerDataStore.getStore().getPlayerData(player);
+
+    if (playerDataResult.isError()) {
+      player.sendMessage(
+          "Failed to get player data (report this to an admin): " + playerDataResult.getError());
+      return;
+    }
+
+    PlayerData playerData = playerDataResult.getValue();
+    String checkpointName = playerData.getCurrentCheckpointName();
+
+    if (checkpointName == "FINISH") {
+      return;
+    }
+
+    Checkpoint nextCheckpoint = findNextCheckpoint(checkpointName);
+
+    if (nextCheckpoint == null) {
+      player.sendMessage(
+          ChatColor.RED
+              + "Failed to get the  checkpoint (report this to an admin): "
+              + checkpointName);
+      return;
+    }
+
+    player.setCompassTarget(nextCheckpoint.shape.getCenter());
+  }
+
+  public Checkpoint findNextCheckpoint(String currentCheckpointName) {
+    for (Checkpoint checkpoint : checkpoints) {
+      if (checkpoint.previousCheckpointName == null) {
+        continue;
+      }
+      if (checkpoint.previousCheckpointName.equals(currentCheckpointName)) {
+        return checkpoint;
+      }
+    }
+    return null;
   }
 }
